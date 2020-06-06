f7cc2008d2dd6ca051e221ea9f606fad6d5197f5
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2020-06-06 08:54:01 +0200

    Add first draft of text editor

---
+++ src/components/Editor.tsx
@@ -0,0 +1,37 @@
+import React, { useRef, useEffect, useState } from "react";
+import Quill from "quill";
+
+export const TextEditor = () => {
+  const [editor, setEditor] = useState<Quill>();
+  const [text, setText] = useState("");
+  const editorRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    let Inline = Quill.import("blots/inline");
+    class GrammarlyInline extends Inline {}
+    GrammarlyInline.tagName = "G";
+    GrammarlyInline.blotName = "grammarly-inline";
+    GrammarlyInline.className = "gr_";
+    Quill.register(GrammarlyInline);
+
+    const container = editorRef.current as Element;
+    setEditor(new Quill(container));
+  }, []);
+
+  const saveText = () => {
+    if (editor) {
+      setText(editor.getText());
+    }
+  };
+
+  useEffect(() => {
+    console.log(text);
+  }, [text]);
+
+  return (
+    <>
+      <button onClick={saveText}>Save text</button>
+      <div ref={editorRef}></div>
+    </>
+  );
+};
